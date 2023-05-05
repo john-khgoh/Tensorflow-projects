5807a7b41afc0bda7a9560c68c8af858214db6c5
john-khgoh <john.khgoh@gmail.com>
2023-05-05 22:14:00 +0800

    added test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,14 @@
 #import tensorflow as tf
-import numpy as np
 from os import getcwd
 import pandas as pd
-from time import sleep
+import numpy as np
 import re
+from sklearn.impute import KNNImputer, SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.pipeline import Pipeline, make_pipeline
 
-pd.set_option('display.max_columns', 100)
+#pd.set_option('display.max_columns', 100)
 #pd.set_option('display.max_rows', 10000)
 
 wd = getcwd()
@@ -71,10 +74,72 @@ numerical_df_cols_list = list(numerical_df.columns)
 categorical_number_list = find_substring_from_list('year',numerical_df_cols_list)
 categorical_number_list += find_substring_from_list('yr',numerical_df_cols_list)
 categorical_number_list += find_substring_from_list('mo',numerical_df_cols_list)
-categorical_number_list = list(set(categorical_number_list)) #unique
+categorical_number_list = list(set(categorical_number_list))
 
 categorical_df_2 = numerical_df.filter(categorical_number_list)
 categorical_df = pd.concat([categorical_df,categorical_df_2],axis=1)
 numerical_df = numerical_df.drop(columns=categorical_number_list)
+categorical_cols =  categorical_df.columns
+numerical_cols = numerical_df.columns
 
-print(numerical_df)
+#Unique value by column and count of each value
+
+for i in categorical_cols:
+    #print(categorical_df[i].value_counts())
+    break
+
+numerical_cols = numerical_df.columns
+for i in numerical_cols:
+    #print(numerical_df[i].value_counts())
+    break
+
+#Distribution of NAs by column
+cat_col_na_dict = {}
+for i in categorical_cols:
+    cat_col_na_dict[i] = categorical_df[i].isna().sum()
+
+num_col_na_dict = {}
+for i in numerical_cols:
+    num_col_na_dict[i] = numerical_df[i].isna().sum()
+
+#Sort the dictionary by value
+sorted_num_col_na_dict = dict(sorted(num_col_na_dict.items(), key=lambda item: item[0]))
+sorted_cat_col_na_dict = dict(sorted(cat_col_na_dict.items(), key=lambda item: item[0]))
+#print(sorted_num_col_na_dict)
+#print(sorted_cat_col_na_dict)
+
+#Columns that cannot be imputed because NaN is part of the data description
+legal_na_cols_list = ['Alley','MasVnrType','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinType2','FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond','PoolQC','Fence','MiscFeature']
+categorical_df_3 = categorical_df.filter(legal_na_cols_list)
+categorical_df = categorical_df.drop(columns=legal_na_cols_list) #The other NaNs in categorical_df can be imputed
+
+#Pipeline:
+#Caterorical_df_3: One-hot Encoder
+#Categorical_df: Imputation -> One-hot encoder
+#Numerical_df: Imputation -> Normalize
+
+categorical_col_list = list(categorical_df.columns)
+numerical_col_list = list(numerical_df.columns)
+'''
+cat_pipeline = make_pipeline(
+    SimpleImputer(strategy='most_frequent'),
+    OneHotEncoder(handle_unknown='ignore')
+)
+
+num_pipeline = make_pipeline(
+    SimpleImputer(strategy='median'),
+    StandardScaler()
+)
+
+cat3_pipeline = make_pipeline(
+    OneHotEncoder(handle_unknown='ignore')
+)
+
+ct = ColumnTransformer([
+    ("num",num_pipeline,numerical_col_list),
+    ("cat",cat_pipeline,categorical_col_list),
+    ("cat3",cat3_pipeline,legal_na_cols_list),
+])
+
+prepared_data = ct.fit_transform(train_df)
+'''
