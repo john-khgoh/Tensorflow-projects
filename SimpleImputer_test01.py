8f2d80206d1ebac461f22210c7ddd5809f16a706
john-khgoh <john.khgoh@gmail.com>
2023-05-10 19:51:59 +0800

    updated the SimpleImputer

--- SimpleImputer_test01.py
+++ SimpleImputer_test01.py
@@ -1,4 +1,4 @@
-#An experimental Keras SimpleImputer layer
+#An experimental Keras SimpleImputer layer as a wrapper for sklearn's SimpleImputer
 
 import tensorflow as tf
 import pandas as pd
@@ -6,12 +6,20 @@ import numpy as np
 
 class SimpleImputer(tf.keras.layers.Layer):
     def __init__(self,strategy='mean',**kwargs):
+        from sklearn.impute import SimpleImputer
         super().__init__(**kwargs)
         self.strategy = strategy
+        self.imputer = SimpleImputer(strategy=strategy)
+        self.result = None
     
     def call(self,data):
+        self.result = self.imputer.fit(data)
+        self.result = self.imputer.transform(data)
+        self.result = tf.convert_to_tensor(self.result)
+        return self.result
+        '''
         data = tf.convert_to_tensor(data)
-        print(data)
+        allowed_strategies = ["mean", "median", "most_frequent", "constant"]
         if(self.strategy=='constant'):
             pass
         elif(self.strategy=='mean'):
@@ -21,11 +29,12 @@ class SimpleImputer(tf.keras.layers.Layer):
         elif(self.strategy=='most_frequent'):
             pass
         else:
-            raise Error()
-    
+            raise Exception('Can only use these strategies:%s' %allowed_strategies)
+        '''
     def get_config(self):
         config = super(SimpleImputer,self).get_config()
         config.update({
+            "strategy": self.strategy
         })
         return config
 
@@ -40,4 +49,4 @@ df = pd.concat([a_df,b_df],axis=1)
 c = np.random.randn(2,5)
 
 x = SimpleImputer('mean')
-x.call(c)
+print(x.call(df))
